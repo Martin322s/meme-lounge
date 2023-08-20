c78d7041b5303360e121fa7def53cb6185204746
Martin Sofroniev <117728421+Martin322s@users.noreply.github.com>
2023-08-20 17:38:45 +0300

    catalog component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Footer } from './components/common/Footer';
 import { Home } from './components/Home';
 import { Login } from './components/Login';
 import { Register } from './components/Register';
+import { Catalog } from './components/Catalog';
 
 function App() {
 	return (
@@ -15,6 +16,7 @@ function App() {
 						<Route path="/" element={<Home />} />
 						<Route path="/login" element={<Login />} />
 						<Route path="/register" element={<Register />} />
+						<Route path="/catalog" element={<Catalog />} />
 					</Routes>
 				</main>
 			</div>
