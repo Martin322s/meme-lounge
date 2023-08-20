ce1b436c2573998819ff9874a56f87c1417cc51e
Martin Sofroniev <117728421+Martin322s@users.noreply.github.com>
2023-08-20 17:31:28 +0300

    register component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Navigation } from './components/common/Navigation';
 import { Footer } from './components/common/Footer';
 import { Home } from './components/Home';
 import { Login } from './components/Login';
+import { Register } from './components/Register';
 
 function App() {
 	return (
@@ -13,6 +14,7 @@ function App() {
 					<Routes>
 						<Route path="/" element={<Home />} />
 						<Route path="/login" element={<Login />} />
+						<Route path="/register" element={<Register />} />
 					</Routes>
 				</main>
 			</div>
