d0389b7702a2be61af2e48123a05acc435471cec
Martin Sofroniev <117728421+Martin322s@users.noreply.github.com>
2023-08-23 19:41:22 +0300

    login/logout user functions

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -1,10 +1,15 @@
 import { createContext } from "react";
+import { useStorage } from "../hooks/useStorage";
 
 export const AuthContext = createContext();
 
 export const AuthContextProvider = ({ children }) => {
+    const [auth, setAuth] = useStorage('session', {});
+    const loginUser = (data) => setAuth(data);
+    const logoutUser = () => setAuth({});
+
     return (
-        <AuthContext.Provider>
+        <AuthContext.Provider value={{ user: auth, loginUser, logoutUser }}>
             {children}
         </AuthContext.Provider>
     );
