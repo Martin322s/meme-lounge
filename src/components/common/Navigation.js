594282474f26c73fccdc9b7a7e04b719bbd5b18a
Martin Sofroniev <117728421+Martin322s@users.noreply.github.com>
2023-08-23 21:35:57 +0300

    change user/guest navigation

--- src/components/common/Navigation.js
+++ src/components/common/Navigation.js
@@ -1,26 +1,37 @@
+import { useContext } from "react";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../../contexts/AuthContext";
 
 export const Navigation = () => {
+    const { user } = useContext(AuthContext);
+
     return (
         <nav>
             <Link to="/catalog">All Memes</Link>
 
-            <div className="user">
-                <Link to="/create-meme">Create Meme</Link>
-                <div className="profile">
-                    <span>Welcome, {'email'}</span>
-                    <Link to="/profile">My Profile</Link>
-                    <Link to="/logout">Logout</Link>
-                </div>
-            </div>
-
-            <div className="guest">
-                <div className="profile">
-                    <Link to="/login">Login</Link>
-                    <Link to="/register">Register</Link>
-                </div>
-                <Link className="active" to="/">Home Page</Link>
-            </div>
+            {user?.accessToken
+                ?
+                <>
+                    <div className="user">
+                        <Link to="/create-meme">Create Meme</Link>
+                        <div className="profile">
+                            <span>Welcome, {user?.email}</span>
+                            <Link to="/profile">My Profile</Link>
+                            <Link to="/logout">Logout</Link>
+                        </div>
+                    </div>
+                </>
+                :
+                <>
+                    <div className="guest">
+                        <div className="profile">
+                            <Link to="/login">Login</Link>
+                            <Link to="/register">Register</Link>
+                        </div>
+                        <Link className="active" to="/">Home Page</Link>
+                    </div>
+                </>
+            }
         </nav>
     );
 };
