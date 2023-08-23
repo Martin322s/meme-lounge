56dc8a96e71acabccfb145937270785ea2bb29f5
Martin Sofroniev <117728421+Martin322s@users.noreply.github.com>
2023-08-23 20:06:43 +0300

    login and register service

---
+++ src/services/authService.js
@@ -0,0 +1,6 @@
+import requester from "./requester";
+
+const baseUrl = 'http://localhost:3030/users';
+
+export const registerUser = (data) => requester.post(`${baseUrl}/register`, {}, data);
+export const loginUser = (data) => requester.post(`${baseUrl}/login`, {}, data);
