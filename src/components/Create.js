999d7087442cecc5f5d78fcd314450776f80b37e
Martin Sofroniev <marti.sofroniev0408@gmail.com>
2023-08-28 15:21:43 +0300

    controlled create meme component

--- src/components/Create.js
+++ src/components/Create.js
@@ -1,22 +1,45 @@
+import { useState } from "react";
+
 export const Create = () => {
+    const [data, setData] = useState({
+        title: '',
+        description: '',
+        imageUrl: ''
+    });
+
+    const changeHandler = (e) => {
+        setData(state => ({
+            ...state,
+            [e.target.name]: e.target.value
+        }));
+    }
+
+    const submitHandler = (e, values) => {
+        e.preventDefault();
+        console.log(values);
+    }
+
     return (
         <section id="create-meme">
-            <form id="create-form">
+            <form id="create-form" onSubmit={(e) => submitHandler(e, data)}>
                 <div className="container">
                     <h1>Create Meme</h1>
                     <label htmlFor="title">Title</label>
-                    <input 
-                        id="title" 
-                        type="text" 
-                        placeholder="Enter Title" 
-                        name="title" 
+                    <input
+                        id="title"
+                        type="text"
+                        placeholder="Enter Title"
+                        name="title"
+                        value={data.title}
+                        onChange={(e) => changeHandler(e)}
                     />
                     <label htmlFor="description">Description</label>
                     <textarea
                         id="description"
                         placeholder="Enter Description"
                         name="description"
-                        defaultValue={""}
+                        value={data.description}
+                        onChange={(e) => changeHandler(e)}
                     />
                     <label htmlFor="imageUrl">Meme Image</label>
                     <input
@@ -24,6 +47,8 @@ export const Create = () => {
                         type="text"
                         placeholder="Enter meme ImageUrl"
                         name="imageUrl"
+                        value={data.imageUrl}
+                        onChange={(e) => changeHandler(e)}
                     />
                     <input
                         type="submit"
