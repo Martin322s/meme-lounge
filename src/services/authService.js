4510f80f8214f25e42f9650f24688e60e0f21774
Martin Sofroniev <marti.sofroniev0408@gmail.com>
2023-08-28 14:58:49 +0300

    logout service call

--- src/services/authService.js
+++ src/services/authService.js
@@ -4,3 +4,5 @@ const baseUrl = 'http://localhost:3030/users';
 
 export const registerUser = (data) => requester.post(`${baseUrl}/register`, {}, data);
 export const loginUser = (data) => requester.post(`${baseUrl}/login`, {}, data);
+export const logoutUser = (token) =>
+    requester.get(`${baseUrl}/logout`, { 'X-Authorization': token });
